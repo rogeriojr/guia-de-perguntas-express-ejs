1c522d098bea8b0ce923877baa6ae4daa20f0ff9
rogeriojr <rogeriojr1100@gmail.com>
2024-03-27 15:01:48 -0300

    database connect auth

---
+++ database/database.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+require('dotenv').config();
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const express = require('express'); // importando o express
+const express = require('express'); // Importando o express
+const bodyParser = require('body-parser');
+const connection = require('./database/database'); // Conexão com o banco de dados
+
 const app = express(); // Iniciando o express
-const porta = 4000; // Defina a porta na qual o servidor irá escutar
+const porta = 4000; // Definindo a porta na qual o servidor irá escutar
 
-//Configurando engine
-app.set('view engine', 'ejs') // Seta ao Express como EJS como view engine
-app.use(express.static('public'))
+// Conexão com o banco de dados
+connection.authenticate()
+    .then(() => {
+        console.log("Conexão com o banco de dados feita com sucesso!");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
+
+// Configurando engine
+app.set('view engine', 'ejs'); // Definindo o EJS como view engine
+// Arquivos estáticos
+app.use(express.static('public'));
+// Configurações de formulário com body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-//Rota para renderizar a página (forma dinamica por params)
-app.get('/', function(req, res) { // Requisição feita na raiz
-    res.render('index')
+//ROTAS 
+// Rota para renderizar a página inicial
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
+// Rota para renderizar a página de perguntas
 app.get('/perguntar', function(req, res) {
-    res.render('perguntar')
+    res.render('perguntar');
+});
+
+// Rota para salvar pergunta
+app.post('/salvarpergunta', function(req, res) {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send('Formulário recebido - Título: ' + titulo + ', Descrição: ' + descricao);
 });
 
 // Tratamento de erro para rota padrão
@@ -22,6 +47,7 @@ app.use(function(req, res) {
     res.status(404).send('Página não encontrada');
 });
 
+// Iniciando o servidor
 app.listen(porta, function(erro) {
     if (erro) {
         console.log('Ocorreu um erro ao iniciar o servidor:', erro);
