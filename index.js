4c96fcc35e9e010a00079778a146ec8213d37a00
rogeriojr <rogeriojr1100@gmail.com>
2024-03-27 12:24:46 -0300

    add bootstrap and view questions

--- index.js
+++ index.js
@@ -5,49 +5,27 @@ const porta = 4000; // Defina a porta na qual o servidor irá escutar
 
 //Configurando engine
 app.set('view engine', 'ejs') // Seta ao Express como EJS como view engine
+app.use(express.static('public'))
 
-//Rota para renderizar a página (hard code)
-// app.get('/', function(req, res){ // Requisição feita na raiz
-//   var nome = 'Rogério Jr';
-//   var lang = 'Javascript';
-//   var empresa = 'RJ-DEV';
-//   var clientes = 10;
-
-//   res.render('index', {
-//     nome: nome,
-//     lang: lang,
-//     empresa: empresa,
-//     clientes: clientes
-//   });
-// });
 
 //Rota para renderizar a página (forma dinamica por params)
-app.get('/:nome?/:lang?', function(req, res){ // Requisição feita na raiz
-  var nome = req.params.nome; //recebe o param na req
-  var lang = req.params.lang; //recebe o param na req
-  var empresa = 'RJ-DEV';
-  var clientes = 10;
-  
-  var exibirMsg = false
+app.get('/', function(req, res) { // Requisição feita na raiz
+    res.render('index')
+});
 
-  res.render('index', {
-    nome: nome,
-    lang: lang,
-    empresa: empresa,
-    clientes: clientes,
-    msg: exibirMsg
-  });
+app.get('/perguntar', function(req, res) {
+    res.render('perguntar')
 });
 
 // Tratamento de erro para rota padrão
 app.use(function(req, res) {
-  res.status(404).send('Página não encontrada');
+    res.status(404).send('Página não encontrada');
 });
 
 app.listen(porta, function(erro) {
-  if (erro) {
-    console.log('Ocorreu um erro ao iniciar o servidor:', erro);
-  } else {
-    console.log('Servidor iniciado com sucesso na porta', porta);
-  }
+    if (erro) {
+        console.log('Ocorreu um erro ao iniciar o servidor:', erro);
+    } else {
+        console.log('Servidor iniciado com sucesso na porta', porta);
+    }
 });
